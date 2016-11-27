460c13f0182a542ac4f8e6c874c620873cc82faa
vvaks <vvaks@hortonworks.com>
2016-11-27 15:03:14 -0500

    update

--- BIOLOGICS_DEMO_CONTROL/package/scripts/master.py
+++ BIOLOGICS_DEMO_CONTROL/package/scripts/master.py
@@ -11,27 +11,27 @@ class DemoControl(Script):
         os.makedirs(params.install_dir)
     os.chdir(params.install_dir)
     Execute('git clone ' + params.download_url)
-    os.chdir(params.install_dir+'DataSimulators/DeviceSimulator')
+    os.chdir(params.install_dir+'/DataSimulators/DeviceSimulator')
     Execute('mvn clean package')
-    os.chdir(params.install_dir+'DataSimulators/DeviceSimulator/target')
+    os.chdir(params.install_dir+'/DataSimulators/DeviceSimulator/target')
     shutil.copy('DeviceSimulator-0.0.1-SNAPSHOT-jar-with-dependencies.jar', params.install_dir)
 
   def start(self, env):
     print 'Start Simulation';
-    Execute('nohup java -jar DeviceSimulator-0.0.1-SNAPSHOT-jar-with-dependencies.jar BioReactor 1000 Simulation $NIFI_HOST_IP > BioReactor_1000_Sim.log 2>&1 & echo $! > BioReactor_1000_Sim.pid')
+    Execute('nohup java -jar '+params.install_dir+'/DeviceSimulator-0.0.1-SNAPSHOT-jar-with-dependencies.jar BioReactor 1000 Simulation $NIFI_HOST_IP > BioReactor_1000_Sim.log 2>&1 & echo $! > BioReactor_1000_Sim.pid')
 
-    Execute('nohup java -jar DeviceSimulator-0.0.1-SNAPSHOT-jar-with-dependencies.jar FiltrationSystem 1000 Simulation $NIFI_HOST_IP > Filtration_1000_Sim.log 2>&1 & echo $! > Filtration_1000_Sim.pid')
+    Execute('nohup java -jar '+params.install_dir+'/DeviceSimulator-0.0.1-SNAPSHOT-jar-with-dependencies.jar FiltrationSystem 1000 Simulation $NIFI_HOST_IP > Filtration_1000_Sim.log 2>&1 & echo $! > Filtration_1000_Sim.pid')
     
   def stop(self, env):
     print 'Stop Simulation';
-    Execute (format('kill -9 `cat '+params.install_dir+' BioReactor_1000_Sim.pid` >/dev/null 2>&1')) 
-    Execute (format('kill -9 `cat '+params.install_dir+' Filtration_1000_Sim.pid` >/dev/null 2>&1')) 
+    Execute (format('kill -9 `cat '+params.install_dir+'BioReactor_1000_Sim.pid` >/dev/null 2>&1')) 
+    Execute (format('kill -9 `cat '+params.install_dir+'Filtration_1000_Sim.pid` >/dev/null 2>&1')) 
 
-    Execute ('rm -f '+params.install_dir+' BioReactor_1000_Sim.pid')
-    Execute ('rm -f '+params.install_dir+' Filtration_1000_Sim.pid')
+    Execute ('rm -f '+params.install_dir+'BioReactor_1000_Sim.pid')
+    Execute ('rm -f '+params.install_dir+'Filtration_1000_Sim.pid')
     
   def status(self, env):
-    check_process_status(params.install_dir+' BioReactor_1000_Sim.pid')
+    check_process_status(params.install_dir+'BioReactor_1000_Sim.pid')
     
   def configure(self, env):
     import params
